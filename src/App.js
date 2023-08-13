7dc74fc44a1b19cf0d3b024dc41cbcf061fa8309
hatem782 <hatembenechikh100@gmail.com>
2023-08-13 19:20:35 +0100

    eng

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Speech from "./Speech/Speech.ar";
+import Speech from "./Speech/Speech";
 import { useState } from "react";
 
 function App() {
